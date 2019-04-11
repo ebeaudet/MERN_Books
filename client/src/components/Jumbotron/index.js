9dfb17897c92bf9d2ef7ed94d71b3544fdf3edaa
unknown <ed.beaudet@gmail.com>
2019-04-10 21:09:15 -0400

    final push

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./style.css";
 
 function Jumbotron() {
   return (
-    <div className="jumbotron text-center">
-      <h1><span><a id="jumboHead" href="/">Ed's Bookshelf</a></span></h1>
-        <div className="tossing">
+    <div className="jumbotron text-center text-white">
+      <h1><span>Ed's Bookshelf </span></h1>
+        <div className="tossing"> 
           <a target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
             Brought to You by Google Books
           </a>
--- client/src/components/RemoveBookBtn/index.js
+++ client/src/components/RemoveBookBtn/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import Button from "../Button";
 
-
-class RemoveBookBtn extends React.Component {
-
-    render(){
-        return (
-            <Button className="deleteBtn" onClick={()=>this.props.delete(this.props.id)}>
-                Delete Book   
-            </Button>
-        )
-    }
-    
+function RemoveBookBtn(props) {
+    return (
+      <Button className="delete-btn" {...props} role="button" tabIndex="0">
+        Delete
+      </Button>
+    );
   }
+ 
 
   export default RemoveBookBtn;
